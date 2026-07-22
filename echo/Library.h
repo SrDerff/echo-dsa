a748ae5af0104a7e7550b45b47796b258aa1c1d7
Derff <srderfflinqer@gmail.com>
2026-07-22 02:09:00 -0500

    library

--- echo/Library.h
+++ echo/Library.h
@@ -1,1 +1,29 @@
 #pragma once
+#include "HashTable.h"
+#include "Song.h"
+
+class Library {
+private:
+	HashTable<int, Song>allSongs;
+	int visibleRows;
+	int topRowIndex;
+	int currentRowIndex;
+	int currentSongId;
+public:
+	Library() : visibleRows(17), topRowIndex(17), currentRowIndex(1) {
+
+	}
+
+	HashTable<int, Song>& getAllSongs() { return allSongs; }
+	int getVisibleRows() { return visibleRows; }
+	int topRowIndex() { return topRowIndex; }
+	int currentRowIndex() { return currentRowIndex; }
+
+	Song& getSongById(int idSong) {
+		return allSongs.getElement(idSong);
+	}
+
+	int getCurrentSongId() {
+		return currentSongId;
+	}
+};
